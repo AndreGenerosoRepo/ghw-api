1bab025af9d573f37e8ff47bcccd008c54153c05
André Generoso <114500148+AndreGenerosoRepo@users.noreply.github.com>
2023-04-03 23:12:42 +0100

    Update app.py
    api

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+hackathons = {
+    "GHW: API Week": {
+        "start_date": "2023-04-03 12:00:00",
+        "end_date": "2023-04-10 12:00:00",
+        "location": "Everywhere, Online",
+        "type": "Digital Only"
+    },
+    "Bitcamp": {
+        "start_date": "2023-04-07 12:00:00",
+        "end_date": "2023-04-09 12:00:00",
+        "location": "College Park, Maryland",
+        "type": "In-Person Only"
+    }
+}
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello , thank you 404 last commit nooottttt!!!</p>"
+    return "<p>Hello , thank you 404 last commit !!!</p>"
+
+@app.route('/getHackatons', methods=['GET'])
+def getHackatons():
+    return hackathons
 
 if __name__=="__main__":
     app.run(debug = True)
